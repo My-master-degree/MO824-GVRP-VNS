f723f300cc5356ea739c87654794f5b75bf95698
Matheus Diogenes Andrade <ra228117@barbuda.lab.ic.unicamp.br>
2019-04-25 20:24:53 -0300

    First

---
+++ src/metaheuristics/vns/AbstractVNS.java
@@ -0,0 +1,157 @@
+/**
+ * 
+ */
+package metaheuristics.vns;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import problems.Evaluator;
+import solutions.Solution;
+
+/**
+ * Abstract class for metaheuristic GRASP (Greedy Randomized Adaptive Search
+ * Procedure). It consider a maximization problem.
+ * 
+ * @author ccavellucci, fusberti
+ * @param <E>
+ *            Generic type of the element which composes the solution.
+ */
+public abstract class AbstractVNS<E> {
+
+	/**
+	 * Class used to represent a neighborhood structure 
+	 *
+	 */
+	public abstract class NeighborhoodStructure {
+		
+		/**
+		 * Returns an random solution from a the neighborhood structure space
+		 * 
+		 * @return An random solution.
+		 */
+		public abstract Solution<E> randomSolution(Solution<E> solution);
+		
+		/**
+		 * Returns the local optimal solution from a the neighborhood structure space
+		 * 
+		 * @return The local optimal solution.
+		 */
+		public abstract Solution<E> localOptimalSolution(Solution<E> solution);
+	}
+	
+	/**
+	 * flag that indicates whether the code should print more information on
+	 * screen
+	 */
+	public static boolean verbose = true;
+
+	/**
+	 * a random number generator
+	 */
+	static Random rng = new Random(0);
+	
+	/**
+	 * the neighborhood structure list
+	 */
+	protected List<NeighborhoodStructure> neighborhoodStructures;
+	
+	/**
+	 * the objective function being optimized
+	 */
+	protected Evaluator<E> ObjFunction;
+
+	/**
+	 * the best solution cost
+	 */
+	protected Double bestCost;
+
+	/**
+	 * the best solution
+	 */
+	protected Solution<E> bestSol;
+
+	/**
+	 * the number of iterations the VNS main loop executes.
+	 */
+	protected Integer maxNumberOfIterations;
+	
+	/**
+	 * the time in microseconds the VNS main loop executes.
+	 */
+	protected Double maxDurationInMilliseconds;
+
+	/**
+	 * Creates a new solution which is empty, i.e., does not contain any
+	 * element.
+	 * 
+	 * @return An empty solution.
+	 */
+	public abstract Solution<E> createEmptySol();
+
+	/**
+	 * Constructor for the AbstractGRASP class.
+	 * 
+	 * @param objFunction
+	 *            The objective function being maximized.
+	 * @param alpha
+	 *            The GRASP greediness-randomness parameter (within the range
+	 *            [0,1])
+	 * @param iterations
+	 *            The number of iterations which the GRASP will be executed.
+	 */
+	public AbstractVNS(Evaluator<E> objFunction, Integer iterations, Double maxDurationInMilliseconds) {
+		this.ObjFunction = objFunction;
+		this.maxNumberOfIterations = iterations;
+		this.maxDurationInMilliseconds = maxDurationInMilliseconds;
+	}
+
+	/**
+	 * The GRASP constructive heuristic, which is responsible for building a
+	 * feasible solution by selecting in a greedy-random fashion, candidate
+	 * elements to enter the solution.
+	 * 
+	 * @return A feasible solution to the problem being maximized.
+	 */
+	public abstract Solution<E> constructiveHeuristic();
+	
+	/**
+	 * The GRASP mainframe. It consists of a loop, in which each iteration goes
+	 * through the constructive heuristic and local search. The best solution is
+	 * returned as result.
+	 * 
+	 * @return The best feasible solution obtained throughout all iterations.
+	 */
+	public Solution<E> solve() {
+		bestSol = constructiveHeuristic();
+		long startTime = System.currentTimeMillis();
+		for (int i = 0, j = 0; i < this.neighborhoodStructures.size() && j < maxNumberOfIterations && System.currentTimeMillis() > startTime + maxDurationInMilliseconds; i++, j++) {
+//			get random solution
+			Solution<E> randomSolution = this.neighborhoodStructures.get(i).randomSolution(this.bestSol);
+			this.ObjFunction.evaluate(randomSolution);
+//			get local optimal solution
+			Solution<E> localOptimalSolution = localSearch(randomSolution);			
+			this.ObjFunction.evaluate(localOptimalSolution);
+//			check cost
+			if (localOptimalSolution.cost > bestSol.cost) {
+				bestSol = new Solution<E>(localOptimalSolution);
+				if (verbose) {
+					System.out.println("(Iter. " + j + ") BestSol = " + bestSol);
+				}
+			}		
+		}
+
+		return bestSol;
+	}
+
+	/**
+	 * The GRASP local search phase is responsible for repeatedly applying a
+	 * neighborhood operation while the solution is getting improved, i.e.,
+	 * until a local optimum is attained.
+	 * 
+	 * @return An local optimum solution.
+	 */
+	public abstract Solution<E> localSearch(Solution<E> solution);	
+
+}
---
+++ src/problems/gvrp/GVRP.java
@@ -0,0 +1,120 @@
+package problems.gvrp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+import java.util.List;
+
+import problems.Evaluator;
+import solutions.Solution;
+
+/**
+ * A quadractic binary function (QBF) is a function that can be expressed as the
+ * sum of quadractic terms: f(x) = \sum{i,j}{a_{ij}*x_i*x_j}. In matricial form
+ * a QBF can be expressed as f(x) = x'.A.x 
+ * The problem of minimizing a QBF is NP-hard [1], even when no constraints
+ * are considered.
+ * 
+ * [1] Kochenberger, et al. The unconstrained binary quadratic programming
+ * problem: a survey. J Comb Optim (2014) 28:58–81. DOI
+ * 10.1007/s10878-014-9734-0.
+ * 
+ * @author ccavellucci, fusberti
+ *
+ */
+public class GVRP implements Evaluator<List<Integer>> {
+
+	/**
+	 * Dimension of the domain.
+	 */
+	public Integer size;
+
+	/**
+	 * The graph vertices, containing customers and fuel stations.
+	 */
+	public Integer[] vertices;
+
+	/**
+	 * The customers demands.
+	 */
+	public Integer[] customersDemands;
+	
+	/**
+	 * The matrix of distance among the graph nodes
+	 */
+	public Double[][] distanceMatrix;
+
+	/**
+	 * The constructor for Green-VRP class. The filename of the
+	 * input for setting matrix of distance, customers and fuel stations data. 	
+	 * 
+	 * @param filename
+	 *            Name of the file containing the input for setting the GVRP.
+	 * @throws IOException
+	 *             Necessary for I/O operations.
+	 */
+	public GVRP(String filename) throws IOException {
+		readInput(filename);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see problems.Evaluator#getDomainSize()
+	 */
+	@Override
+	public Integer getDomainSize() {
+		return size;
+	}
+
+	/**
+	 * {@inheritDoc} In the case of a GVRP, the evaluation correspond to
+	 * computing the sum of arcs used in the solution.
+	 * 
+	 * @return The evaluation of the GVRP.
+	 */
+	@Override
+	public Double evaluate(Solution<List<Integer>> sol) {
+
+		return sol.cost = 0d;
+
+	}
+
+	/**
+	 * Responsible for setting the QBF function parameters by reading the
+	 * necessary input from an external file. this method reads the domain's
+	 * dimension and matrix {@link #distanceMatrix}.
+	 * 
+	 * @param filename
+	 *            Name of the file containing the input for setting the black
+	 *            box function.
+	 * @return The dimension of the domain.
+	 * @throws IOException
+	 *             Necessary for I/O operations.
+	 */
+	protected Integer readInput(String filename) throws IOException {
+		Reader fileInst = new BufferedReader(new FileReader(filename));
+		StreamTokenizer stok = new StreamTokenizer(fileInst);
+
+		stok.nextToken();
+		Integer _size = (int) stok.nval;
+		distanceMatrix = new Double[_size][_size];
+
+		for (int i = 0; i < _size; i++) {
+			for (int j = i; j < _size; j++) {
+				stok.nextToken();
+				distanceMatrix[i][j] = stok.nval;
+				//A[j][i] = A[i][j];
+				if (j>i)
+					distanceMatrix[j][i] = 0.0;
+			}
+		}
+
+		return _size;
+
+	}
+
+}
