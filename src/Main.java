657cc36e38c3cd4267d7c433daf3d3521bc76709
Matheus Diógenes Andrade <matheusdiogenesandrade@gmail.com>
2019-05-08 13:13:48 -0300

    Writing instance file

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,42 @@
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import problems.gvrp.instances.InstancesGenerator;
-import problems.qbf.solvers.*;
-import solutions.Solution;
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
+
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
-		InstancesGenerator.generate(20, 100, 30, 20, 20);
+		Integer numOfCustomers = 50;
+		Double vehicleAutonomy = 30d;
+		Map<InstancesGenerator.Node, Integer> nodes = InstancesGenerator.generate(numOfCustomers, 500, vehicleAutonomy, 20, 20);
+		
+		nodes = nodes.entrySet().stream().sorted(comparingByValue()).collect(
+		            toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+		                LinkedHashMap::new));
+		
+		
+		
+		String content = "NUM_CUSTOMERS="+numOfCustomers+"\nVEHICLE_AUTONOMY="+vehicleAutonomy+"\n";
+		for (InstancesGenerator.Node node : nodes.keySet()) {
+			Integer id = nodes.get(node);
+			if (id == 0)
+				content += "DEPOT=" + node.x + "," + node.y + "\n";
+			else if (id > 0 && id < numOfCustomers + 1)
+				content += "CUSTOMER_"+id+"=" + node.x + "," + node.y + "\n";
+			else
+				content += "AFS_"+id+"=" + node.x + "," + node.y + "\n";
+		}		
+	    BufferedWriter writer = new BufferedWriter(new FileWriter("c20a30.txt"));
+	    writer.write(content);
+	     
+	    writer.close();
 	}
 
 }
--- src/problems/gvrp/instances/InstancesGenerator.java
+++ src/problems/gvrp/instances/InstancesGenerator.java
@@ -8,8 +8,8 @@ import java.util.Random;
 
 public class InstancesGenerator {
 	public static class Node{
-		int x;
-		int y;
+		public int x;
+		public int y;
 		Node (int x, int y){
 			this.x = x;
 			this.y = y;
@@ -52,11 +52,10 @@ public class InstancesGenerator {
 //		depot		
 		Node depot = new Node (random.nextInt(planeDimension), random.nextInt(planeDimension));		
 		nodes.put(depot, 0);
-//		System.out.println("Depot generated at "+depot.x+" "+depot.y);		
+		System.out.println("Depot generated at "+depot.x+" "+depot.y);		
 		for (int i = 1; i <= customersNumber; i++) {
-//			System.out.println("Generating customer "+ i);
-			Node customer;
-//			int limit = afsUsed == rechargeStationsNumber ? (int) Math.floor(vehicleAutonomy/2) : (int) (vehicleAutonomy * 1.5);
+			System.out.println("Generating customer "+ i);
+			Node customer;			
 			int limit = (int) (vehicleAutonomy * 1.5);
 			do {				
 //				X
@@ -74,38 +73,15 @@ public class InstancesGenerator {
 				else
 					y += random.nextInt(Math.min(planeDimension - depot.y, limit) + 1);
 				customer = new Node (x, y);				
-//				System.out.println("\tTrying generate customer "+ i);
+				System.out.println("\tTrying generate customer "+ i);
 			}while(nodes.get(customer) != null);
-//			System.out.println("\tCustomer "+ i+" generated at "+customer.x+" "+customer.y);			
-			nodes.put(customer, i);
-//			put some AFS in the middle
-//			if (getDistance(depot, customer) > (vehicleAutonomy / 2)) {				
-//				Node afs;
-//				
-//				do {	
-//					int x, y;
-//					if (customer.x < depot.x) {
-//						x = (int) (Math.max(depot.x - vehicleAutonomy, 0) + random.nextInt(customer.x - depot.x + (int) (3*vehicleAutonomy/2)) + 1); 
-//					}else {
-//						x = (int) (Math.max(depot.x - vehicleAutonomy, 0) + random.nextInt(customer.x - depot.x + (int) (3*vehicleAutonomy/2)) + 1);
-//					}
-//					afs = new Node(Math.min(customer.x, depot.x) + random.nextInt(Math.abs(customer.x - depot.x) + 1), 
-//							Math.min(customer.y, depot.y) + random.nextInt(Math.abs(customer.y - depot.y) + 1));
-////					afs = new Node(random.nextInt(planeDimension), random.nextInt(planeDimension));
-//					System.out.println("\tTrying to put some AFS in "+afs.x+", "+afs.y+" between depot and customer "+ i);
-//					System.out.println("\tDistance between depot and afs is "+getDistance(depot, afs));
-//					System.out.println("\tDistance between cusotmer and afs is "+getDistance(afs, customer));
-//				}while(nodes.get(afs) != null || getDistance(depot, afs) > vehicleAutonomy || getDistance(afs, customer) > vehicleAutonomy/2);
-//				afsUsed++;
-//				nodes.put(afs, customersNumber + 1 + afsUsed);				
-//				System.out.println("\tAFS inserted between depot and customer "+i+" at "+afs.x+" "+afs.y);				
-//			} 
+			System.out.println("\tCustomer "+ i+" generated at "+customer.x+" "+customer.y);			
+			nodes.put(customer, i);			
 		}
 //		generate random points to be possibly AFS's
-		int maxAFSsNumber = customersNumber * 100,
+		int maxAFSsNumber = customersNumber * 50,
 			limit = (int) Math.floor(vehicleAutonomy);
 		Map<Node, Map<Node, Boolean>> possibleAFSs = new HashMap<Node, Map<Node, Boolean>> (maxAFSsNumber);
-//		System.out.println("Depot at "+depot.x+" "+depot.y);	
 		for (int i = 0; i < maxAFSsNumber; i++) {
 			Node possibleAFS;
 			do {
@@ -113,22 +89,24 @@ public class InstancesGenerator {
 					depot.x + (random.nextInt(2) == 1 ? - random.nextInt(Math.min(limit, depot.x) + 1) : 
 						random.nextInt(Math.min(planeDimension - depot.x, limit) + 1)), 
 					depot.y + (random.nextInt(2) == 1 ? - random.nextInt(Math.min(limit, depot.y) + 1) :
-						random.nextInt(Math.min(planeDimension - depot.y, limit) + 1)));				
+						random.nextInt(Math.min(planeDimension - depot.y, limit) + 1)));		
+				System.out.println("Trying");
 			}while(possibleAFSs.get(possibleAFS) != null || nodes.get(possibleAFS) != null);
-//			System.out.println("Possible AFS at "+possibleAFS.x+" "+possibleAFS.y);
+			System.out.println("Possible AFS at "+possibleAFS.x+" "+possibleAFS.y);
 //			get customers in possible AFS cover
 			Map<Node, Boolean> customersInAFSCover = new HashMap<Node, Boolean>();
-//			System.out.println("Its covering");
+			System.out.println("Its covering");
 			for (Node node:nodes.keySet()) {				
 				if (nodes.get(node) != 0 && getDistance(node, possibleAFS) <= (limit / 2)) { 
 					customersInAFSCover.put(node, true);
-//					System.out.print(nodes.get(node) + " ");
+					System.out.print(nodes.get(node) + " ");
 				}				
 			}
-//			System.out.println();
+			System.out.println();
 			possibleAFSs.put(possibleAFS, customersInAFSCover);
 		}
 //		check
+		System.out.println("Check");
 		for (Node customer:nodes.keySet()) {
 			boolean find = false;
 			if (nodes.get(customer) != 0) {
@@ -143,7 +121,8 @@ public class InstancesGenerator {
 				}
 			}			
 		}
-//		run cover set		
+//		run cover set
+		System.out.println("SET COVER MIP");
 		try {
 	      GRBEnv    env   = new GRBEnv("setCover.log");
 		  GRBModel  model = new GRBModel(env);		
@@ -188,7 +167,7 @@ public class InstancesGenerator {
 		  env.dispose();		
 		} catch (GRBException e) {
 			System.out.println("Error code: " + e.getErrorCode() + ". " + e.getMessage());
-		}
+		}		
 //		return
 		return nodes;
 	}
